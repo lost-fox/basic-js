0405dda268af595213403e2e3f62c8658fe80903
lost-fox <Oljaburak@mail.ru>
2021-09-19 21:36:01 +0300

    feat: done 'transform-array'

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,30 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+
+  if (!Array.isArray(arr)){
+    throw new Error ("'arr' parameter must be an instance of the Array!")
+  }
+
+  let newArr =[];
+
+  for (let i=0;i<arr.length;i++){
+    switch (arr[i]){
+      case '--discard-next': delete arr[i+1]; break;
+      case '--discard-prev': delete newArr[i-1]; break;
+      case '--double-next': newArr.push(arr[i+1]); break;
+      case '--double-prev': newArr.push(arr[i-1]); break;
+      default:newArr.push(arr[i]);
+    }
+  }
+  
+  for(let i=0;i<newArr.length;i++){
+     if(newArr[i]==undefined){
+        newArr.splice(i,1);
+        i--;
+     }
+  }
+
+ return newArr;
 }
